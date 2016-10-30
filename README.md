3bcee4fd8c407c1c1d3a276925f703c478cea87b
j.faassen <j.faassen@linkorb.com>
2016-10-30 12:50:14 +0100

    Initial checkin (tooling)

---
+++ README.md
@@ -0,0 +1,13 @@
+realm-peri22
+============
+
+Deze repository bevat een conversie van de peri22 decor XML bestanden naar [realm](http://github.com/linkorb/realm) formaat.
+
+Online viewer: [https://dataset.perinatologie.nl/peri22](https://dataset.perinatologie.nl/peri22)
+    
+## Updates
+
+Met behulp van het bijgevoegde `generate.sh` script kan de content van deze repository worden
+bijgewerkt/her-gegenereerd obv de bron-bestanden van decor.nictiz.nl/perinatologie
+
+    ./generate.sh
---
+++ generate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -f realm.xml ]
+  then
+    echo "Downloading dependencies"
+  else
+    echo "realm.xml file not found. Are you running this script from the root of the project?"
+    exit 1
+fi
+
+composer install
+if [ -f vendor/bin/realm ]
+  then
+    echo "realm binary ok"
+  else
+    echo "realm binary not found. possible issue with composer?"
+    exit 1
+fi
+mkdir -p dependencies/
+wget -O "dependencies/peri22.decor.xml" "http://decor.nictiz.nl/decor/services/RetrieveTransaction?language=nl-NL&version=2016-02-16T11:05:01&hidecolumns=45ghi&id=2.16.840.1.113883.2.4.3.11.60.90.77.1.5&2013-09-10T00:00:00&format=xml"
+vendor/bin/realm decor:convert -f dependencies/peri22.decor.xml -o .
