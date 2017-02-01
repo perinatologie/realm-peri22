57f34cdedf0f2b1e65b12dcfa750d90b8aa292a6
j.faassen <j.faassen@linkorb.com>
2017-02-01 14:38:29 +0100

    Update source peri22 publication 2016-02-16 to 2016-12-21

--- generate.sh
+++ generate.sh
@@ -16,5 +16,6 @@ if [ -f vendor/bin/realm ]
     exit 1
 fi
 mkdir -p dependencies/
-wget -O "dependencies/peri22.decor.xml" "http://decor.nictiz.nl/decor/services/RetrieveTransaction?language=nl-NL&version=2016-02-16T11:05:01&hidecolumns=45ghi&id=2.16.840.1.113883.2.4.3.11.60.90.77.1.5&2013-09-10T00:00:00&format=xml"
+# wget -O "dependencies/peri22.decor.xml" "http://decor.nictiz.nl/decor/services/RetrieveTransaction?language=nl-NL&version=2016-02-16T11:05:01&hidecolumns=45ghi&id=2.16.840.1.113883.2.4.3.11.60.90.77.1.5&2013-09-10T00:00:00&format=xml"
+wget -O "dependencies/peri22.decor.xml" "http://decor.nictiz.nl/decor/services/RetrieveTransaction?language=nl-NL&version=2016-12-21T15%3A25%3A00&hidecolumns=45ghi&id=2.16.840.1.113883.2.4.3.11.60.90.77.1.5&effectiveDate=2013-09-10T00%3A00%3A00&format=xml"
 vendor/bin/realm decor:convert -f dependencies/peri22.decor.xml -o .
